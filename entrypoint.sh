90f1c5e90fe8f3de1714a63a8b15fb2232b9cdb5
Ilya Antipenko <ilya@antipenko.pp.ua>
2021-10-27 00:46:12 +0200

    Install NPM dependencies before running test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh -l
 
+echo "Installing npm dependencies"
+# Run npm ci to install deps
+npm ci
+
 # "homey app validate" command doesn't return correct exit code in case of validation errors
 RESULT=`homey app validate -l publish`
 echo "$RESULT"
